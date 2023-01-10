9199bd6a6e143718c1ff0800dd8d447979a3d085
Rafael Matthew Tanumihardja <rafaelmatthew35@gmail.com>
2023-01-10 14:47:52 +0700

    commit lab 13

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _932003.Tanumikhardzha.Rafael.lab13.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
